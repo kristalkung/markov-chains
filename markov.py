33faed12821a93393ecfaf2a13974ea229ccddd2
Kristal <kristalkung@gmail.com>
2021-02-05 04:59:32 +0000

    Completed functions: open_and_read_file, text_string; made notes for how to complete make_text

--- markov.py
+++ markov.py
@@ -1,6 +1,6 @@
 """Generate Markov text from text files."""
 
-from random import choice
+import random
 
 
 def open_and_read_file(file_path):
@@ -10,10 +10,11 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    contents = open(file_path).read()
+    cleaned_contents = contents.rstrip().replace('\n',' ')
+    return cleaned_contents
 
-    return 'Contents of your file as one long string'
-
+text_string = open_and_read_file('green-eggs.txt')
 
 def make_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
@@ -42,30 +43,57 @@ def make_chains(text_string):
 
     chains = {}
 
-    # your code goes here
+    list_text = text_string.split(" ")
+
+    for i in range(0, len(list_text) - 2):
+        if (list_text[i], list_text[i + 1]) not in chains:
+            chains[(list_text[i], list_text[i + 1])] = [list_text[i + 2]]
+        else:
+            chains[(list_text[i], list_text[i + 1])].append(list_text[i + 2])
 
     return chains
 
+
+chains = make_chains(text_string)
 
 def make_text(chains):
     """Return text from chains."""
 
     words = []
 
+    current_key = random.choice(list(chains.keys()))
+    # need to put current_key into words
+
+    random_word = random.choice(list(chains[current_key]))    
+    
+    for word in chains[current_key]: # chains[current_key] is value of the key, but that is a list that can multiple items.  
+        words.append(word)
+    
+    # put random_word into words
+    # change current key to the last two words of words
+    # loop again, but only add the new random_word
+    # loop ends when the key doesn't exist
+    # return joined words
+
+
+    print(current_key)
+    print(chains[current_key])
+    
     # your code goes here
 
     return ' '.join(words)
 
+print(make_text(chains))
 
-input_path = 'green-eggs.txt'
+# input_path = 'green-eggs.txt'
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+# # Open the file and turn it into one long string
+# input_text = open_and_read_file(input_path)
 
-# Get a Markov chain
-chains = make_chains(input_text)
+# # Get a Markov chain
+# chains = make_chains(input_text)
 
-# Produce random text
-random_text = make_text(chains)
+# # Produce random text
+# random_text = make_text(chains)
 
-print(random_text)
+# print(random_text)
